153d91b109adaa5e9b1d9f9cb1f8584af4127034
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-02-07 00:19:53 +0900

    [add] Added aws-lambda-functions

---
+++ aws-lambda-functions/src/bin/hello.rs
@@ -0,0 +1,72 @@
+use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+
+/// This is the main body for the function.
+/// Write your code inside it.
+/// There are some code example in the following URLs:
+/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    // Extract some useful information from the request
+    let who = event
+        .query_string_parameters_ref()
+        .and_then(|params| params.first("name"))
+        .unwrap_or("world");
+    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+
+    // Return something that implements IntoResponse.
+    // It will be serialized to the right response event automatically by the runtime
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body(message.into())
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing::init_default_subscriber();
+
+    run(service_fn(function_handler)).await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lambda_http::{Request, RequestExt};
+    use std::collections::HashMap;
+
+    #[tokio::test]
+    async fn test_generic_http_handler() {
+        let request = Request::default();
+
+        let response = function_handler(request).await.unwrap();
+        assert_eq!(response.status(), 200);
+
+        let body_bytes = response.body().to_vec();
+        let body_string = String::from_utf8(body_bytes).unwrap();
+
+        assert_eq!(
+            body_string,
+            "Hello world, this is an AWS Lambda HTTP request"
+        );
+    }
+
+    #[tokio::test]
+    async fn test_http_handler_with_query_string() {
+        let mut query_string_parameters: HashMap<String, String> = HashMap::new();
+        query_string_parameters.insert("name".into(), "aws-lambda-functions".into());
+
+        let request = Request::default().with_query_string_parameters(query_string_parameters);
+
+        let response = function_handler(request).await.unwrap();
+        assert_eq!(response.status(), 200);
+
+        let body_bytes = response.body().to_vec();
+        let body_string = String::from_utf8(body_bytes).unwrap();
+
+        assert_eq!(
+            body_string,
+            "Hello aws-lambda-functions, this is an AWS Lambda HTTP request"
+        );
+    }
+}
