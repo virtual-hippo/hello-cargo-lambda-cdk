b41077c2bbd7dd024b63363bf5fdaa6b02000d26
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-02-07 00:29:09 +0900

    [add][cdk] Added sample rust lambda function

--- cdk/lib/stacks/hello-cargo-lambda-cdk-stack.ts
+++ cdk/lib/stacks/hello-cargo-lambda-cdk-stack.ts
@@ -1,10 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { RustFunction } from "cargo-lambda-cdk";
+
+import * as path from "path";
 
 export interface HelloCargoLambdaCdkStackProps extends cdk.StackProps {}
 
 export class HelloCargoLambdaCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: HelloCargoLambdaCdkStackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props?: HelloCargoLambdaCdkStackProps
+  ) {
     super(scope, id, props);
+
+    new RustFunction(this, "RustFunction", {
+      manifestPath: path.join(
+        __dirname,
+        "../../../",
+        "aws-lambda-functions",
+        "Cargo.toml"
+      ),
+      binaryName: "hello",
+    });
   }
 }
