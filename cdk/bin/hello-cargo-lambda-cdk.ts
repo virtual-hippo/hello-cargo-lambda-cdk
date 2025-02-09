acaf218a6f04e68ed81bf2beac2afdd7444939f5
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-02-09 16:01:34 +0900

    [update][cdk] Modified  env alias (DEV to Dev).

--- cdk/bin/hello-cargo-lambda-cdk.ts
+++ cdk/bin/hello-cargo-lambda-cdk.ts
@@ -6,7 +6,7 @@ import { HelloCargoLambdaCdkPipelineStack } from "../lib/stacks";
 const app = new cdk.App();
 
 new HelloCargoLambdaCdkPipelineStack(app, "HelloCargoLambdaCdkPipeline", {
-  ...PIPELINE_PARAMETERS.DEV,
-  envAlias: "DEV",
-  appParameter: APP_PARAMETERS.DEV,
+  ...PIPELINE_PARAMETERS.Dev,
+  envAlias: "Dev",
+  appParameter: APP_PARAMETERS.Dev,
 });
--- cdk/lib/parameters/parameters.ts
+++ cdk/lib/parameters/parameters.ts
@@ -2,13 +2,13 @@ import { AppParameterType } from "./app-parameter-type";
 import { DEV_PARAMETER, DEV_PIPELINE_PARAMETER } from "./dev-parameter";
 import { PipelineParameterType } from "./pipeline-parameter-type";
 
-export const envs = ["DEV"] as const;
+export const envs = ["Dev"] as const;
 export type Env = (typeof envs)[number];
 
 export const APP_PARAMETERS: Record<Env, AppParameterType> = {
-  DEV: DEV_PARAMETER,
+  Dev: DEV_PARAMETER,
 } as const;
 
 export const PIPELINE_PARAMETERS: Record<Env, PipelineParameterType> = {
-  DEV: DEV_PIPELINE_PARAMETER,
+  Dev: DEV_PIPELINE_PARAMETER,
 } as const;
