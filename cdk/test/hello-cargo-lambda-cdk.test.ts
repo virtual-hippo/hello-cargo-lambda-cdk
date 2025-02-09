5bedf14993676764e3ae6b7e2919000d3427bac6
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-02-09 16:02:24 +0900

    [add][cdk] Added cdk snapshot test

---
+++ cdk/test/hello-cargo-lambda-cdk.test.ts
@@ -0,0 +1,25 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { APP_PARAMETERS, PIPELINE_PARAMETERS } from "../lib/parameters";
+import { HelloCargoLambdaCdkPipelineStack } from "../lib/stacks";
+import { HelloCargoLambdaCdkStage } from "../lib/stages";
+
+test("Snapshot test for Dev stage", () => {
+  const app = new cdk.App();
+  const stage = new HelloCargoLambdaCdkStage(app, "Dev", {
+    appParameter: APP_PARAMETERS.Dev,
+  });
+
+  expect(Template.fromStack(stage.helloCargoLambdaCdkStack)).toMatchSnapshot();
+});
+
+test("Snapshot test for Dev pipeline", () => {
+  const app = new cdk.App();
+  const pipelineStack = new HelloCargoLambdaCdkPipelineStack(app, "HelloCargoLambdaCdkPipeline", {
+    ...PIPELINE_PARAMETERS.Dev,
+    envAlias: "Dev",
+    appParameter: APP_PARAMETERS.Dev,
+  });
+
+  expect(Template.fromStack(pipelineStack)).toMatchSnapshot();
+});
